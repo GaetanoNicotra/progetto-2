ae2315b632360ae03a8af5484677c47ad73bfa00
GaetanoNicotra <nicotragaetano10@gmail.com>
2026-02-01 18:52:55 +0100

    [UPD] creazione della sezione statistiche

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
+import Statistic from "./components/Statistic"
 
 export default function App() {
   return <div className="min-h-screen bg-white font-sans text-slate-900 antialiased selection:bg-indigo-100 selection:text-indigo-700">
@@ -8,6 +9,7 @@ export default function App() {
 
     <main>
       <HeroSection />
+      <Statistic />
     </main>
 
   </div>
---
+++ landing-page/src/components/Statistic.tsx
@@ -0,0 +1,23 @@
+
+const Statistic = () => {
+    return (
+        <div className=" border-slate-100">
+
+            <div className="text-indigo-600 font-semibold text-center ">Trusted by developers</div>
+
+            <div className="font-bold text-4xl text-center mt-3">Built for scale and performance</div>
+
+            <ul className="flex justify-around text-center mt-20">
+                <li className="text-5xl font-bold">500+ <div className="text-sm text-gray-600 font-normal mt-4">Components ready</div></li>
+                <li className="text-5xl font-bold">25k <div className="text-sm text-gray-600 font-normal mt-4">downloads</div></li>
+                <li className="text-5xl font-bold">4.9/5 <div className="text-sm text-gray-600 font-normal mt-4">Customer Rating</div></li>
+                <li className="text-5xl font-bold"> - 24h <div className="text-sm text-gray-600 font-normal mt-4">Support Response</div></li>
+            </ul>
+
+        </div>
+
+
+    )
+}
+
+export default Statistic
